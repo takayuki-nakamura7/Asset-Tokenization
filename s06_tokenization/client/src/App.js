fc2ccd1e03de47f4f4f4a8d125460561016c8f0c
takayuki nakamura <ktkr7195@gmai.com>
2021-05-05 16:00:20 +0900

    Add front end

--- s06_tokenization/client/src/App.js
+++ s06_tokenization/client/src/App.js
@@ -1,31 +1,43 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import MyToken from "./contracts/MyToken.json";
+import MyTokenSale from "./contracts/MyTokenSale.json";
+import KycContract from "./contracts/KycContract.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { loaded: false };
 
   componentDidMount = async () => {
     try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+        // Get network provider and web3 instance.
+        this.web3 = await getWeb3();
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+        // Use web3 to get the user's accounts.
+        this.accounts = await this.web3.eth.getAccounts();
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+        // Get the contract instance.
+        //this.networkId = await this.web3.eth.net.getId(); <<- this doesn't work with MetaMask anymore
+        this.networkId = await this.web3.eth.getChainId();      
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+        this.myToken = new this.web3.eth.Contract(
+          MyToken.abi,
+          MyToken.networks[this.networkId] && MyToken.networks[this.networkId].address,
+        );
+
+        this.myTokenSale = new this.web3.eth.Contract(
+          MyTokenSale.abi,
+          MyTokenSale.networks[this.networkId] && MyTokenSale.networks[this.networkId].address,
+        );
+        this.kycContract = new this.web3.eth.Contract(
+          KycContract.abi,
+          KycContract.networks[this.networkId] && KycContract.networks[this.networkId].address,
+        );
+
+        // Set web3, accounts, and contract to the state, and then proceed with an
+        // example of interacting with the contract's methods.
+        this.setState({ loaded:true });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -49,7 +61,7 @@ class App extends Component {
   };
 
   render() {
-    if (!this.state.web3) {
+    if (!this.state.loaded) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
