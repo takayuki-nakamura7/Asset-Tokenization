39023fe123d2d223bc0f046de6e011884131d970
takayuki nakamura <ktkr7195@gmai.com>
2021-05-05 12:10:29 +0900

    Modify unit tests

---
+++ s06_tokenization/contracts/MyTokenSale.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0;
+
+import "./Crowdsale.sol";
+
+contract MyTokenSale is Crowdsale {
+    
+    constructor(
+        uint256 rate,    // rate in TKNbits
+        address payable wallet,
+        IERC20 token
+    )
+        Crowdsale(rate, wallet, token)
+        public
+    {
+
+    }
+
+}
--- s06_tokenization/test/MyToken.test.js
+++ s06_tokenization/test/MyToken.test.js
@@ -11,12 +11,17 @@ chai.use(chaiAsPromised);
 
 const expect = chai.expect;
 
-contract("Token Test", async accounts => {
+require('dotenv').config({path: '../.env'});
+
+contract("Token Test", function(accounts) {
     const [ initialHolder, recipient, anotherAccount ] = accounts;
 
+    beforeEach(async () => {
+      this.myToken = await Token.new(process.env.INITIAL_TOKENS);
+    });
 
     it("All tokens should be in my account", async () => {
-    let instance = await Token.deployed();
+    let instance = this.myToken;
     let totalSupply = await instance.totalSupply();
     //old style:
     //let balance = await instance.balanceOf.call(initialHolder);
@@ -27,7 +32,7 @@ contract("Token Test", async accounts => {
 
     it("I can send tokens from Account 1 to Account 2", async () => {
       const sendTokens = 1;
-      let instance = await Token.deployed();
+      let instance = this.myToken;
       let totalSupply = await instance.totalSupply();
       expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
       expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;      
@@ -37,7 +42,7 @@ contract("Token Test", async accounts => {
 
 
     it("It's not possible to send more tokens than account 1 has", async () => {
-      let instance = await Token.deployed();
+      let instance = await this.myToken;
       let balanceOfAccount = await instance.balanceOf(initialHolder);
 
       expect(instance.transfer(recipient, new BN(balanceOfAccount+1))).to.eventually.be.rejected;
